b0530f4f7bddbe0515302cb2cbdbf665c21683fb
slbraun <slbraun@stanford.edu>
2025-12-05 13:59:19 -0800

    Update blochscript.js

--- blochscript.js
+++ blochscript.js
@@ -262,7 +262,7 @@ btnCz.addEventListener("click", () => {
 // ----------------------------------------------------
 createBlochSphere("bloch-sphere-0", "cam-phi-0", "cam-theta-0");
 createBlochSphere("bloch-sphere-1", "cam-phi-1", "cam-theta-1");
-// ↓↓↓ paste below this line ↓↓↓
+
 initCustomMatrix();
 
 //test
